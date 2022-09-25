971c26d037aa366d1f28894e650e938cd8e394d3
Adrin7113 <adrinjosect@gmail.com>
2022-09-25 11:34:20 +0530

    "final commit(prolly)"

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,22 @@
-import React, { useState , useEffect} from 'react';
+import React , { useState , useEffect} from 'react';
 import './Nav.css'
 
 function Nav() {
-    const [show , handleShow] = useState(false);
-
-
+    const [show,handleShow] = useState(false)
     useEffect(() => {
-      window.addEventListener("scroll" , () => {
-        if (window.scrollY > 100) {
-            handleShow(true);
-        }   else {handleShow(false);}
-      });  
-      return () => {
-        window.removeEventListener("scroll");
-      };
-    }, []);
+      window.addEventListener("scroll", function listener(){
+        if(window.scrollY > 100){
+          handleShow(true)
+        }else handleShow(false)
+        
+        return () => {
+          window.removeEventListener("scroll",listener)
+        }
+      })
+    },[])
     
   return (
-    <div className={`nav ${show && "nav_black"}`}>
+    <div className={`nav ${show && 'nav-black'}`}>
       <img
       className='nav-logo'
       src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
 import "./Row.css";
+// import Youtube from "react-youtube";
+// import movieTrailer from "movie-trailer";
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    // const [trailerUrl, setTrailerUrl] = useState("");
 
     //snippet which runs on a specific condition
     useEffect(() => {
@@ -17,7 +20,34 @@ function Row({ title, fetchUrl, isLargeRow }) {
         }
         fetchData();
     }, [fetchUrl]);
-   
+    
+    // const opts = {
+    //   height: "390",
+    //   width: "100%",
+    //   playerVars: {
+    //     // https://developers.google.com/youtube/player_parameters
+    //     autoplay: 1,
+    //   },
+    // }
+
+//     const handleClick = (movie) => {
+//         if (trailerUrl)
+//         {
+//           setTrailerUrl("");
+//         }
+//         else {
+//           movieTrailer(null ,{ tmdbId: movie.id })
+//                    .then((url)=>{
+//                      console.log("url is "+url);
+//                      const urlParams=new URLSearchParams(new URL(url).search);
+//                      console.log("urlParamsn"+urlParams);
+//                      setTrailerUrl(urlParams.get("v"));
+//                    })
+//                    .catch((error)=> console.log(error));
+//     }
+//  };
+
+
   return (
     <div className="row">
       
@@ -28,11 +58,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map(movie =>(
           <img 
           key={movie.id}
+          // onClick={() => handleClick(movie)}
           className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
           src={`${baseUrl}${movie.backdrop_path}`==="https://image.tmdb.org/t/p/original/null" ? `${baseUrl}${movie.poster_path}` : `${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
         ))}
         </div>
-
+          {/* {trailerUrl && <Youtube videoId={'trailerUrl'} opts={opts} />} */}
     </div>
   );
 }
